28ec695a9ea8b5d6399172623bdfb78a851d5f79
Ari <s220238775@deakin.edu.au>
2026-05-07 16:41:48 +1000

    07/05 UI Fixes / File Updates

--- 81/app/src/main/java/com/example/a81/network/ChatRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.a81.network;
-
-public class ChatRequest {
-    public String message;
-
-    public ChatRequest(String message) {
-        this.message = message;
-    }
-}
--- 81/app/src/main/java/com/example/a81/network/ChatResponse.java
+++
@@ -1,5 +0,0 @@
-package com.example.a81.network;
-
-public class ChatResponse {
-    public String reply;
-}
